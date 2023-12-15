6c316afc3333d0b75adaaf7a24204a8b189898a1
BGM-109 <vividxxxxx@gmail.com>
2023-12-15 12:14:15 +0900

    first commit

---
+++ lib/bootstrap.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  runApp(
+    ProviderScope(child: await builder()),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:riverpod_counter/app/app.dart';
+import 'package:riverpod_counter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
